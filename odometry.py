9f92be2ab9dfeb44028f8e50cc236d2053d63a2b
atahanyp <151150634+atahanyp@users.noreply.github.com>
2024-04-19 17:33:38 +0300

    Update odometry.py

--- odometry.py
+++ odometry.py
@@ -8,7 +8,7 @@ import math
 class OdometryCalculator:
     def __init__(self):
         rospy.init_node('odometry_calculator')
-
+        
         self.last_time = rospy.Time.now()
 
         self.current_x = 0.0
@@ -21,7 +21,6 @@ class OdometryCalculator:
 
         self.wheel_radius = 0.135  
         
-
         self.imu_sub = rospy.Subscriber('/imu1/data', Imu, self.imu_callback)
         self.left_encoder_sub = rospy.Subscriber('/drive_system_left_motors_feedbacks', Float64MultiArray, self.left_encoder_callback)
         self.right_encoder_sub = rospy.Subscriber('/drive_system_right_motors_feedbacks', Float64MultiArray, self.right_encoder_callback)
@@ -29,7 +28,6 @@ class OdometryCalculator:
 
     def imu_callback(self, msg):
         orientation_q = msg.orientation
-
         orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w] # converting quaternion to euler 
         roll, pitch, yaw = euler_from_quaternion(orientation_list)
         self.current_theta = yaw
@@ -38,33 +36,25 @@ class OdometryCalculator:
     def left_encoder_callback(self, msg):
         current_time = rospy.Time.now()
         dt = (current_time - self.last_time).to_sec()
-
-        # Sol motor rpm verisini hıza çevirme
-        self.velocity_left = msg.data[0]
-        
+        # Converting left motor rpm data to velocity
+        self.velocity_left = msg.data[0]        
         self.velocity_left = 2 * math.pi * self.wheel_radius * (self.velocity_left / 60)
-
         self.current_x += self.velocity_left * dt * cos(self.current_theta)
         self.current_y += self.velocity_left * dt * sin(self.current_theta)      # Setting the location points on x and y axis'
-
         self.last_time = current_time  
-
         self.publish_odometry()
 
     def right_encoder_callback(self, msg):
         current_time = rospy.Time.now()
         dt = (current_time - self.last_time).to_sec()
-
-        # Sağ motor rpm verisini hıza çevirme
+        # Converting right motor rpm data to velocity
         self.velocity_right = msg.data[0]
         
         self.velocity_right= 2 * math.pi * self.wheel_radius * (self.velocity_right / 60)
 
         self.current_x += self.velocity_right * dt * cos(self.current_theta)
         self.current_y += self.velocity_right * dt * sin(self.current_theta)    # Setting the location points on x and y axis'
-
         self.last_time = current_time
-
         self.publish_odometry()
 
     def publish_odometry(self):
@@ -86,12 +76,11 @@ class OdometryCalculator:
         odom.twist.twist.linear.x = (self.velocity_left + self.velocity_right) / 2.0
         odom.twist.twist.linear.y = (self.velocity_left + self.velocity_right) / 2.0      
         odom.twist.twist.angular.z = self.angular
-
         self.odom_pub.publish(odom)
 
     def spin(self):
-        rate = rospy.Rate(10)  # 10 Hz
-
+        rate = rospy.Rate(10)  
+        
         while not rospy.is_shutdown():
             self.publish_odometry()
             rate.sleep()
